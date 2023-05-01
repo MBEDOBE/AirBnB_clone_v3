30fa5e6a80d20bb555ef009b27ca34b106ae8b1d
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-01 21:29:02 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""The api's application module"""
+""" Start flask app """
 
+
+import os
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-from models import storage
-import os
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
-CORS(app, origins=["0.0.0.0"])
-
-
-@app.errorhandler(404)
-def not_found(exception):
-    """Handles 404 errors"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Method to handle teardown context"""
+def close_db(error):
+    """ close storage """
     storage.close()
 
 
-if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST') or '0.0.0.0'
-    port = os.getenv('HBNB_API_PORT') or 5000
-    app.run(host=host, port=port, threaded=True, debug=True)
+@app.errorhandler(404)
+def page_not_found(error):
+    """page not found error handling"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    # Set the host, port using environ variables, or defaults if not defined
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+
+    # Run the Flask server with threaded=True to allow concurrent requests
+    app.run(host=host, port=port, threaded=True)
