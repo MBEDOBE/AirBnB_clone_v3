2b8c4123be55c4c556310affaa1d7378ce3b512d
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-01 10:05:26 +0000

    added 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 @app.errorhandler(404)
 def not_found(exception):
     """Takes care of 404 errors"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'NOT FOUND'}), 404)
 
 @app.teardown_appcontext
 def close_storage(exception):
