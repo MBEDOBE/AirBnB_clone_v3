936f02985f4a974e24ffb159d7bb19306e2d9096
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-01 10:21:43 +0000

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""Init file for package views"""
 
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+app_views = Blueprint('app_views', __name__, template_folder='templates')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
