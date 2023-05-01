1ddd59c7c5375cc2237031c8dbf14738c782dac0
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-01 20:50:46 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,23 +2,19 @@
 '''
     app for registering blueprint and starting flask
 '''
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+import os
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 
 app = Flask(__name__)
 CORS(app, origins="0.0.0.0")
-app.register_blueprint(app_views)
-
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
-def tear_down(self):
-    '''
-    close query after each session
-    '''
+def close_storage(exception):
+    """Method to handle teardown context"""
     storage.close()
 
 
@@ -29,7 +25,7 @@ def not_found(error):
     '''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = os.getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True, debug=True)
