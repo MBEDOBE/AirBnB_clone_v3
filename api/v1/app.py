127d3c78734af982ee650588519c0c53bde92c2f
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-01 20:16:26 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """The api's application module"""
 
-import os
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
+from models import storage
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 CORS(app, origins=["0.0.0.0"])
 
+
 @app.errorhandler(404)
 def not_found(exception):
-    """Takes care of 404 errors"""
-    return make_response(jsonify({'error': 'NOT FOUND'}), 404)
+    """Handles 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.teardown_appcontext
 def close_storage(exception):
