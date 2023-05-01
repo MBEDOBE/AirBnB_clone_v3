f02682f1ad7b3e8a08544f078c7a327ef38f286f
Teddystone08 <62164400+Teddystone08@users.noreply.github.com>
2023-05-01 12:24:28 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,30 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+
+models={'amenities': Amenity,
+       'place': Place,
+       'user' : User,
+       'state' : User,
+       'state' : State,
+       'city' : City,
+       'review' : review
+       }
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify(status="OK")
+
+@app_views.route('/stats', methods=['GET'])
+def get_count();
+stats = {}
+for name, model in models.item():
+total = storage.count(model)
+stats[name] = total
+return jsonify(stats)
