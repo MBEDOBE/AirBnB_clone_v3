8766251d3f88d919101db79fc1d0866e3145e524
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-01 22:10:48 +0000

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-    
+
     def get(self, cls, id):
         """
         Returns the object based on the class and its id,
